0fb0eb9cdb2f1aebcf73b3faeffefe3225a921c9
vp <notejde@gmail.com>
2016-09-25 01:42:09 -0700

    add test1 & Client.cs

---
+++ Objects/Client.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  public class ClientTests
+  {
+    private string _firstName;
+    private string _lastName;
+    private int _id;
+    //add StylistId
+    //add constructor
+    public Client(string firstName, string lastName, int id =0)
+  }
+}

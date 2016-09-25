a8d687fd221414f0b870ebe3be02a282c69cea22
vp <notejde@gmail.com>
2016-09-25 01:31:47 -0700

    add test1

--- Tests/ClientTests.cs
+++ Tests/ClientTests.cs
@@ -14,6 +14,14 @@ namespace HairSalon
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
     }
 
+    [Fact]
+    public void T1_IsDatabaseEmpty()
+    {
+      //Act
+      int result = Client.GetAll().Count;
+      //Assert
+      Assert.Equal(0, result);
+    }
     //Dispose() add once write Delete()
     //public void Dispose()
     // {

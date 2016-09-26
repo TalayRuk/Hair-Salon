6604719b377f3f31043ebf969401c7544a2fd7e4
vp <notejde@gmail.com>
2016-09-26 01:30:32 -0700

    add update HomeModule

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -16,6 +16,36 @@ namespace HairSalon
         return View["index.cshtml", AllStylists];
       };
 
+      Get["/clients"] = _ => {
+        List<Client> AllClients = Client.GetAll();
+        return View["client.cshtml", AllClients];
+      };
+
+      Get["/stylists"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["stylist.cshtml", AllStylists];
+      };
+
+      Get["/stylists/new"] = _ => {
+        return View["stylists_form.cshtml"];
+      };
+
+      Post["/stylists/new"] = _ => {
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
+        newStylist.Save();
+        return View["success.cshtml"];
+      };
+
+      Get["/clients/new"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["clients_form.cshtml"];
+      };
+
+      Post["/clients/new"] = _ => {
+        Client newClient = new Client(Request.Form["client-name"], Request.Form["client-phone"], Request.Form["stylist-id"]);
+        newClient.Save();
+        return View["success.cshtml"];
+      };
     }
   }
 }
